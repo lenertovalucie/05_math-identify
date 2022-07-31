368aeb0f1f8f6027e364ff149154f70ae0c20125
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-07-31 09:52:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Spusťte program, vložte do konzole požadované vstupy a odpovězte na otázky níže!
+
+print("Enter a number:")
+num = int(input())  # vložte do konzole hodnotu 10
+num2 = float(input("Enter a number:\n"))  # vložte do konzole hodnotu 2
+
+result = num / num2
+result2 = 5 + 6 / 2
+print(result)
+print(result2)
+
+##### Úkol - Prozkoumej kód
+# Co bude vytištěno do konzole po provedení příkazu na řádku 5?
+# Odpověď:
+
+# Jakého typu bude hodnota proměnné num a num2?
+# Odpověď:
+
+# Jakého typu bude hodnota proměnné result a jakého typu result2?
+# Odpověď:
+
+# Proč příkaz print(result2) vytiskne 8.0 a ne 5.5?
+# Odpověď:
+
+# Co se stane, pokud po spuštění programu dám jako uživatelský vstup hodnotu "hi" místo 10? Vyzkoušejte!
+# Odpověď:
